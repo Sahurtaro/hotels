60c877dadf646154716dabe1320f5afda5d3f7bf
santiago hurtado <sahurtaro@gmail.com>
2024-06-25 19:45:11 -0500

    64. Param middleware

--- app.js
+++ app.js
@@ -9,26 +9,6 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json()); //the data from the boddy is added to the request object using this middleware
 
-const hotels = [
-  {
-    id: 1,
-    name: 'Hotel1',
-    region: 'suroccidente',
-    city: 'cali',
-    zone: 'centro',
-    category: 'hotel',
-    stars: 4,
-    type: 'urbano',
-    web: 'hotel1.com',
-    image: 'www.imagen.png',
-    address: 'calle 1 con carrera 2 num 123',
-    location: {
-      lat: 424,
-      lng: 963,
-    },
-  },
-];
-
 // app.get('/api/v1/hotels', getAllHotels);
 // app.get('/api/v1/hotels/:id', getHotel);
 // app.post('/api/v1/hotels', createHotel);
--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,4 +1,37 @@
 //Route handlers
+
+const hotels = [
+  {
+    id: 1,
+    name: 'Hotel1',
+    region: 'suroccidente',
+    city: 'cali',
+    zone: 'centro',
+    category: 'hotel',
+    stars: 4,
+    type: 'urbano',
+    web: 'hotel1.com',
+    image: 'www.imagen.png',
+    address: 'calle 1 con carrera 2 num 123',
+    location: {
+      lat: 424,
+      lng: 963,
+    },
+  },
+];
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Hotel id is:${val}`);
+
+  if (req.params.id * 1 > hotels.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllHotels = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -29,8 +62,7 @@ exports.updateHotel = (req, res) => {
 };
 
 exports.deleteHotel = (req, res) => {
-  console.log(`deleted hotel`);
-  const hotelId = req.params.id;
+  this.checkID(req, res, next, val);
   res.status(204).json({
     status: 'success',
     data: null,
