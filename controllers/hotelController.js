08d6665d4b62ac25a156a60e26e3e2168e5a5046
santiago hurtado <sahurtaro@gmail.com>
2024-07-02 16:49:57 -0500

    99. Pagination

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -25,13 +25,45 @@ const Hotel = require('./../models/hotelModel');
 exports.getAllHotels = async (req, res) => {
   try {
     //Build query
+    //1A) Filtering
     const queryObj = { ...req.query };
 
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Hotel.find(queryObj);
+    //1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Hotel.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').joint(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numHotels = await Hotel.countDocuments();
+      if (skip >= numHotels) throw new Error('This page does not exist');
+    }
 
     //Execute query
     const hotels = await query;
