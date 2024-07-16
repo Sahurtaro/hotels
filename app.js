042ed120af0152126a6aa66f3fb6c8c45aef833f
santiago hurtado <sahurtaro@gmail.com>
2024-07-16 15:36:49 -0500

    global erro handling middleware

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json()); //the data from the boddy is added to the request object using this middleware
 
-// app.get('/api/v1/hotels', getAllHotels);
-// app.get('/api/v1/hotels/:id', getHotel);
-// app.post('/api/v1/hotels', createHotel);
-// app.patch('/api/v1/hotels/:id', updateHotel);
-// app.delete('/api/v1/hotels/:id', deleteHotel);
-
 //3) Routes
 
 //this is called "mounting the router"
@@ -34,5 +28,22 @@ app.use('/api/v1/providers', providerRouter);
 app.use('/api/v1/destinations', destinationRouter);
 app.use('/api/v1/pilars', pilarRouter);
 app.use('/api/v1/directors', directorRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
