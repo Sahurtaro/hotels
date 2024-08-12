a47e7460bc4a83ade914e3b28894e6982576e5df
santiago hurtado <sahurtaro@gmail.com>
2024-08-12 16:01:10 -0500

    Features: creating the protect function for testing authorization on user login route

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,13 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = (err) =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = (err) => {
+  new AppError('Your token has expired! Please log in again.', 401);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -63,7 +70,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (err.stack.substr(0, 15) === 'ValidationError')
       error = handleValidationErrorDB(error);
-
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
     sendErrorProd(error, res);
   }
 };
